d4cd0d52edd509ce2f49a2ef9f1495bc492ecdc2
fobtac <japodacajoffe@gmail.com>
2023-10-05 00:18:48 -0600

    commit two

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Header from './src/components/Header.js'
+import Nav from './src/components/Nav.js'
+import Main from './src/components/Main.js'
+import Footer from './src/components/Footer.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      homepage
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
